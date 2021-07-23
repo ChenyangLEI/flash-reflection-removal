b036b546761cba274add87d4ab5a9637c975561c
Chenyang LEI <935747833@qq.com>
2021-07-23 11:58:21 +0800

    Fix the bug of download data

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
-# wget "https://hkustconnect-my.sharepoint.com/:u:/g/personal/cleiaa_connect_ust_hk/EWv1afaxrhFKlbT7iX0b8FMB8R1ZeNyUWRQM__A_SPkVGQ?e=8IbhE6&download=1"
+wget "https://hkustconnect-my.sharepoint.com/:u:/g/personal/cleiaa_connect_ust_hk/EWv1afaxrhFKlbT7iX0b8FMB8R1ZeNyUWRQM__A_SPkVGQ?e=8IbhE6&download=1"
 mv 'EWv1afaxrhFKlbT7iX0b8FMB8R1ZeNyUWRQM__A_SPkVGQ?e=8IbhE6&download=1.1'  data.zip
 unzip data.zip
 rm data.zip
